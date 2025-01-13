dfc51d7b57f88d733ddff21febc68d393f32c6d7
Shantanu Balse <infoshantanu02@gmail.com>
2025-01-13 18:28:16 +0000

    Use constant WHITE instead of 0xFFFFFFFF

--- code/mesh.h
+++ code/mesh.h
@@ -24,23 +24,23 @@ inline constexpr u8 NUM_CUBE_FACES = 6 * 2; // NOTE(sbalse): 6 cube faces, 2 tri
 inline constexpr Face CUBE_FACES[NUM_CUBE_FACES] =
 {
     // front
-    { .m_A = 1, .m_B = 2, .m_C = 3, .m_AUV = { 0, 1 }, .m_BUV = { 0, 0 }, .m_CUV = { 1, 0 }, .m_Color = 0xFFFFFFFF },
-    { .m_A = 1, .m_B = 3, .m_C = 4, .m_AUV = { 0, 1 }, .m_BUV = { 1, 0 }, .m_CUV = { 1, 1 }, .m_Color = 0xFFFFFFFF },
+    { .m_A = 1, .m_B = 2, .m_C = 3, .m_AUV = { 0, 1 }, .m_BUV = { 0, 0 }, .m_CUV = { 1, 0 }, .m_Color = WHITE },
+    { .m_A = 1, .m_B = 3, .m_C = 4, .m_AUV = { 0, 1 }, .m_BUV = { 1, 0 }, .m_CUV = { 1, 1 }, .m_Color = WHITE },
     // right
-    { .m_A = 4, .m_B = 3, .m_C = 5, .m_AUV = { 0, 1 }, .m_BUV = { 0, 0 }, .m_CUV = { 1, 0 }, .m_Color = 0xFFFFFFFF },
-    { .m_A = 4, .m_B = 5, .m_C = 6, .m_AUV = { 0, 1 }, .m_BUV = { 1, 0 }, .m_CUV = { 1, 1 }, .m_Color = 0xFFFFFFFF },
+    { .m_A = 4, .m_B = 3, .m_C = 5, .m_AUV = { 0, 1 }, .m_BUV = { 0, 0 }, .m_CUV = { 1, 0 }, .m_Color = WHITE },
+    { .m_A = 4, .m_B = 5, .m_C = 6, .m_AUV = { 0, 1 }, .m_BUV = { 1, 0 }, .m_CUV = { 1, 1 }, .m_Color = WHITE },
     // back
-    { .m_A = 6, .m_B = 5, .m_C = 7, .m_AUV = { 0, 1 }, .m_BUV = { 0, 0 }, .m_CUV = { 1, 0 }, .m_Color = 0xFFFFFFFF },
-    { .m_A = 6, .m_B = 7, .m_C = 8, .m_AUV = { 0, 1 }, .m_BUV = { 1, 0 }, .m_CUV = { 1, 1 }, .m_Color = 0xFFFFFFFF },
+    { .m_A = 6, .m_B = 5, .m_C = 7, .m_AUV = { 0, 1 }, .m_BUV = { 0, 0 }, .m_CUV = { 1, 0 }, .m_Color = WHITE },
+    { .m_A = 6, .m_B = 7, .m_C = 8, .m_AUV = { 0, 1 }, .m_BUV = { 1, 0 }, .m_CUV = { 1, 1 }, .m_Color = WHITE },
     // left
-    { .m_A = 8, .m_B = 7, .m_C = 2, .m_AUV = { 0, 1 }, .m_BUV = { 0, 0 }, .m_CUV = { 1, 0 }, .m_Color = 0xFFFFFFFF },
-    { .m_A = 8, .m_B = 2, .m_C = 1, .m_AUV = { 0, 1 }, .m_BUV = { 1, 0 }, .m_CUV = { 1, 1 }, .m_Color = 0xFFFFFFFF },
+    { .m_A = 8, .m_B = 7, .m_C = 2, .m_AUV = { 0, 1 }, .m_BUV = { 0, 0 }, .m_CUV = { 1, 0 }, .m_Color = WHITE },
+    { .m_A = 8, .m_B = 2, .m_C = 1, .m_AUV = { 0, 1 }, .m_BUV = { 1, 0 }, .m_CUV = { 1, 1 }, .m_Color = WHITE },
     // top
-    { .m_A = 2, .m_B = 7, .m_C = 5, .m_AUV = { 0, 1 }, .m_BUV = { 0, 0 }, .m_CUV = { 1, 0 }, .m_Color = 0xFFFFFFFF },
-    { .m_A = 2, .m_B = 5, .m_C = 3, .m_AUV = { 0, 1 }, .m_BUV = { 1, 0 }, .m_CUV = { 1, 1 }, .m_Color = 0xFFFFFFFF },
+    { .m_A = 2, .m_B = 7, .m_C = 5, .m_AUV = { 0, 1 }, .m_BUV = { 0, 0 }, .m_CUV = { 1, 0 }, .m_Color = WHITE },
+    { .m_A = 2, .m_B = 5, .m_C = 3, .m_AUV = { 0, 1 }, .m_BUV = { 1, 0 }, .m_CUV = { 1, 1 }, .m_Color = WHITE },
     // bottom
-    { .m_A = 6, .m_B = 8, .m_C = 1, .m_AUV = { 0, 1 }, .m_BUV = { 0, 0 }, .m_CUV = { 1, 0 }, .m_Color = 0xFFFFFFFF },
-    { .m_A = 6, .m_B = 1, .m_C = 4, .m_AUV = { 0, 1 }, .m_BUV = { 1, 0 }, .m_CUV = { 1, 1 }, .m_Color = 0xFFFFFFFF },
+    { .m_A = 6, .m_B = 8, .m_C = 1, .m_AUV = { 0, 1 }, .m_BUV = { 0, 0 }, .m_CUV = { 1, 0 }, .m_Color = WHITE },
+    { .m_A = 6, .m_B = 1, .m_C = 4, .m_AUV = { 0, 1 }, .m_BUV = { 1, 0 }, .m_CUV = { 1, 1 }, .m_Color = WHITE },
 };
 
 // NOTE(sbalse): A struct for dynamic sized meshes. Contains an array of vertices, faces and the
